5eecce05290b7f8426bc2a2740cea49d37e385f1
Pitra <justin.pitra@gmail.com>
2024-10-16 09:52:07 +0200

    clean App.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Mon Application. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Form.tsx
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <main>
+      <p>Ceci est le composant formulaire.</p>
+    </main>
+  );
+};
+
+export default Form;
