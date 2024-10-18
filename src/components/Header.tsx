8c668cfb125a342e8382b351b1c61fc7aafebf0c
Pitra <justin.pitra@gmail.com>
2024-10-18 13:02:42 +0200

    styling progress bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
-import "./component-css/Header.css";
+import "./component-css/Header-css/Header.css";
 
 type HeaderProps = {
   step: number;
   totalSteps: number;
 };
 
+const stepDescriptions = [
+  "Account Type",
+  "Personal Information",
+  "Profile Data",
+];
+
 const Header = ({ step, totalSteps }: HeaderProps) => {
   const renderStepIndicator = () => {
     const indicators = [];
     for (let i = 1; i <= totalSteps; i++) {
+      const descriptionClass = i === 2 ? "two-lines" : "";
+      const containerClass = i !== 2 ? "adjusted-step" : "";
       indicators.push(
-        <div key={i} className="step-container">
-          <div className={`line`} />
+        <div key={i} className={`step-container ${containerClass}`}>
           <div className={`step-indicator ${i === step ? "active-step" : ""}`}>
             <span
               className={`step-text ${i === step ? "active-step-text" : ""}`}
@@ -19,7 +26,9 @@ const Header = ({ step, totalSteps }: HeaderProps) => {
               {i}
             </span>
           </div>
-          <div className={`line`} />
+          <span className={`step-description ${descriptionClass}`}>
+            {stepDescriptions[i - 1]}
+          </span>
         </div>
       );
     }
